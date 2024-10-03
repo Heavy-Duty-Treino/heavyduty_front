24163c271361633d0460fa8f016cb149e98afb65
RHamudi <ramonramos.silva16@gmail.com>
2024-10-03 16:06:38 -0300

    feat: adding model exercicio

---
+++ lib/App/interactor/services/models/exercicio_model.dart
@@ -0,0 +1,18 @@
+class ExercicioModel {
+  final String id;
+  final String nome_exercicio;
+  final String nome_usuario;
+  final String tipo_exercicio;
+  final String musculo_alvo;
+  final List<dynamic> musculos_sinergistas;
+  final String categoria_exercicio;
+
+  ExercicioModel(
+      {required this.id,
+      required this.nome_exercicio,
+      required this.nome_usuario,
+      required this.tipo_exercicio,
+      required this.musculo_alvo,
+      required this.musculos_sinergistas,
+      required this.categoria_exercicio});
+}
