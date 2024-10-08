190452f5fde72c81b5b37f9712a6f5e37ff01b09
RHamudi <ramonramos.silva16@gmail.com>
2024-10-08 07:25:56 -0300

    feat: create card

---
+++ lib/App/components/card_treino.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:heavyduty_front/App/components/base_button.dart';
+
+class CardTreino extends StatefulWidget {
+  const CardTreino({super.key});
+
+  @override
+  State<CardTreino> createState() => _CardTreinoState();
+}
+
+class _CardTreinoState extends State<CardTreino> {
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      color: Color.fromARGB(255, 43, 44, 46),
+      child: SizedBox(
+        height: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Treino de costas'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: BaseButton(title: 'Iniciar', ph: 25, pv: 5),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
