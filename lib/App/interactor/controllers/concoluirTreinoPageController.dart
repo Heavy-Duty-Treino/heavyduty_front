2859ab0d681c342af4d1f3d1c6e8db0f4f46b0fd
RHamudi <ramonramos.silva16@gmail.com>
2024-12-09 11:55:17 -0300

    feat: adding method add serie

--- lib/App/interactor/controllers/concoluirTreinoPageController.dart
+++ lib/App/interactor/controllers/concoluirTreinoPageController.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
+import 'package:heavyduty_front/App/interactor/services/models/treino_concluido_model.dart';
 import 'package:heavyduty_front/App/interactor/services/models/treino_model.dart';
+import 'package:heavyduty_front/App/interactor/services/treino_concluido_service.dart';
+import 'package:heavyduty_front/routes.g.dart';
+import 'package:routefly/routefly.dart';
 
 class ConcoluirTreinoPageController extends GetxController {
   late Rx<Treino> treino;
   var exercicios = <Exercicio>[].obs;
+  final service = TreinoConcluidoService();
 
   void setTreino(Treino model) {
     treino = model.obs;
@@ -27,6 +32,44 @@ class ConcoluirTreinoPageController extends GetxController {
 
     treino.value.exercicios[index].series.add(newSerie);
     treino.refresh();
+  }
+
+  void addSerieValue(
+      String idExercicio, String? tipoSerie, int index, int? kg, int? reps) {
+    Exercicio findExercicio = treino.value.exercicios
+        .firstWhere((exercicio) => exercicio.exercicioId == idExercicio);
+
+    if (tipoSerie != null) {
+      treino.value.exercicios
+          .firstWhere((e) => e.exercicioId == idExercicio)
+          .series[index]
+          .tipoSerie = tipoSerie;
+    }
+
+    if (kg != null) {
+      treino.value.exercicios
+          .firstWhere((e) => e.exercicioId == idExercicio)
+          .series[index]
+          .pesoKg = kg;
+    }
+
+    if (reps != null) {
+      treino.value.exercicios
+          .firstWhere((e) => e.exercicioId == idExercicio)
+          .series[index]
+          .repeticoes = reps;
+    }
+  }
+
+  Future<void> concluir() async {
+    var treinoConcluido = new TreinoConcluido(
+        idTreino: treino.value.id,
+        titulo: treino.value.titulo,
+        nomeUsuario: treino.value.nomeUsuario,
+        fotoPerfilUsuario: treino.value.fotoPerfilUsuario,
+        exercicios: treino.value.exercicios);
+    var response = await service.Create(treinoConcluido);
+    if (response.statusCode == 200) return Routefly.navigate(routePaths.treino);
   }
   // void setSeries(String idExercicio) {
   //   final exercicio = treino.value.exercicios
