942b396b75ee414f3235f021db9c3a0bd2aaaa5c
RHamudi <ramonramos.silva16@gmail.com>
2025-01-08 15:17:23 -0300

    feat: adding get chartvolume data

--- lib/App/interactor/controllers/LoginPageController.dart
+++ lib/App/interactor/controllers/LoginPageController.dart
@@ -1,17 +1,22 @@
 import 'dart:developer';
 
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heavyduty_front/App/interactor/services/login_services.dart';
 import 'package:heavyduty_front/App/interactor/services/models/Login_model.dart';
+import 'package:heavyduty_front/App/interactor/services/user_services.dart';
 import 'package:heavyduty_front/routes.g.dart';
 import 'package:routefly/routefly.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPageController extends GetxController {
+  var userServices = UserServices();
   LoginServices services = LoginServices();
   TextEditingController emailInput = TextEditingController();
   TextEditingController passwordInput = TextEditingController();
+  var chatData = Rxn<dynamic>();
+  var sectionVolume = <PieChartSectionData>[].obs;
 
   void authenticate() async {
     var user = new Login(
@@ -20,6 +25,7 @@ class LoginPageController extends GetxController {
       var auth = await services.authenticate(user);
       saveToken(auth.token, emailInput.value.text);
       if (auth.data == 'Usuario Autenticado') {
+        setChatData();
         Routefly.navigate(routePaths.home);
       }
     } catch (e) {
@@ -38,8 +44,46 @@ class LoginPageController extends GetxController {
     return prefs.getString("authUserToken");
   }
 
-  Future<String?> getName() async {
+  Future<String> getName() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString("authUserName");
+    var email = prefs.getString("authUserName");
+    if (email!.isEmpty) return email;
+    return "";
+  }
+
+  void setChatData() async {
+    final response = await userServices.getChartData();
+    chatData.value = response.data;
+    generateFixedPieChartSections(chatData);
+  }
+
+  void generateFixedPieChartSections(Rxn<dynamic> data) {
+    var costas = data.value['costas'];
+    var peito = data.value['peito'];
+    var quadriceps = data.value['quadriceps'];
+    var posterior = data.value['posterior'];
+
+    sectionVolume.assignAll([
+      PieChartSectionData(
+        value: costas.toDouble(),
+        color: Colors.purple,
+        radius: 70,
+      ),
+      PieChartSectionData(
+        value: peito.toDouble(),
+        color: Colors.blue,
+        radius: 70,
+      ),
+      PieChartSectionData(
+        value: quadriceps.toDouble(),
+        color: Colors.orange,
+        radius: 70,
+      ),
+      PieChartSectionData(
+        value: posterior.toDouble(),
+        color: Colors.green,
+        radius: 70,
+      ),
+    ]);
   }
 }
