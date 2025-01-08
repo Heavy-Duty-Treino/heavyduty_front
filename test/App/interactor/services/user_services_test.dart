4240fb2d396abc56a1f96cb9842805cb97d2baeb
RHamudi <ramonramos.silva16@gmail.com>
2025-01-08 15:05:17 -0300

    feat: new user service

--- test/App/interactor/services/user_services_test.dart
+++ test/App/interactor/services/user_services_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:heavyduty_front/App/interactor/services/login_services.dart';
 import 'package:heavyduty_front/App/interactor/services/models/Login_model.dart';
+import 'package:heavyduty_front/App/interactor/services/user_services.dart';
 
 void main() {
   test("Login", () async {
@@ -8,5 +9,11 @@ void main() {
     var user = new Login(email: "heavyduty@gmail.com", password: "123456789");
     var login = await services.authenticate(user);
     expect(login.data, "Usuario Autenticado");
+  });
+
+  test("Chart Data", () async {
+    final services = UserServices();
+    var chart = await services.getChartData();
+    print(chart.data);
   });
 }
