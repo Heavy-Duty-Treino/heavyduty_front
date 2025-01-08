a16a7e4c39dc6a7fd687fcc0d854119cd1950c2c
RHamudi <ramonramos.silva16@gmail.com>
2025-01-08 15:16:59 -0300

    feat: get sharedpreferences

--- lib/App/interactor/controllers/treinoController.dart
+++ lib/App/interactor/controllers/treinoController.dart
@@ -2,14 +2,15 @@ import 'package:get/get.dart';
 import 'package:heavyduty_front/App/interactor/controllers/LoginPageController.dart';
 import 'package:heavyduty_front/App/interactor/services/models/treino_model.dart';
 import 'package:heavyduty_front/App/interactor/services/treino_services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Treinocontroller {
   final service = TreinoServices();
-  final login = LoginPageController();
   final treinos = <Treino>[].obs;
 
   Future<void> GetAll() async {
-    var email = await login.getName();
+    final prefs = await SharedPreferences.getInstance();
+    var email = prefs.getString("authUserName");
     var result = await service.getAll(email!);
     treinos.assignAll(result);
   }
