9197be06af572c336bcea07a966c466872f417f0
RHamudi <ramonramos.silva16@gmail.com>
2025-04-27 17:24:02 -0300

    feat: adding input component

---
+++ lib/App/components/shared/InputField.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heavyduty_front/App/interactor/controllers/LoginPageController.dart';
+
+class InputField extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final String hintText;
+  final bool obscureText;
+  const InputField(
+      {super.key,
+      required this.textEditingController,
+      required this.hintText,
+      required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      controller: textEditingController,
+      decoration: InputDecoration(
+          labelText: hintText, border: const OutlineInputBorder()),
+    );
+  }
+}
